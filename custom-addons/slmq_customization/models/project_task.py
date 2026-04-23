3a05b2ae4d34d85cb500d3bbff02bed8dc7a6a9f
mubeena <user@user-IdeaPad-3-15ITL6>
2026-04-23 10:33:55 +0530

    Email Templates

--- custom-addons/slmq_customization/models/project_task.py
+++ custom-addons/slmq_customization/models/project_task.py
@@ -5,9 +5,29 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
 
+    @api.model
+    def create(self, vals):
+        task = super().create(vals)
+
+        # Get Project Manager group users
+        group = self.env.ref('project.group_project_manager')
+        users = group.user_ids.filtered(lambda u: u.email)
+
+        template = self.env.ref('slmq_customization.task_notify_pm_template')
+
+        for user in users:
+            template.send_mail(task.id, force_send=True, email_values={'email_to': user.email})
+
+        return task
+
+
     def write(self, vals):
 
         res = super().write(vals)
+
+        # Get Project Manager group users
+        group = self.env.ref('project.group_project_manager')
+        users = group.user_ids.filtered(lambda u: u.email)
         
         template_done = self.env.ref('slmq_customization.task_done_mail_template')
 
@@ -16,13 +36,7 @@ class ProjectTask(models.Model):
             # ✅ 1. Task moved to Done
             if vals.get('state') == '1_done':
 
-                manager = task.project_id.user_id
-                if manager and manager.email:
-
-                    template_done.send_mail(
-                        task.id,
-                        force_send=True,
-                        email_values={'email_to': manager.email}
-                    )
+                for user in users:
+                    template_done.send_mail(task.id, force_send=True, email_values={'email_to': user.email})
 
         return res
