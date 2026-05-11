e3f8a567489cf047f83ec79036ae08e12153e172
mubeena <user@user-IdeaPad-3-15ITL6>
2026-05-11 17:20:44 +0530

    Sale Report

--- custom-addons/invoice_template/models/__init__.py
+++ custom-addons/invoice_template/models/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import account_move
 from . import res_company
+from . import report_remittance
+from . import res_bank
+from . import sale_order
+from . import ir_actions_report
